7a48e0a418d436ae932cd37ab669c3ce1f50fbc7
nithishk-art <nithishkcontacts@gmail.com>
2024-12-24 11:41:52 +0530

    modified the logic function

--- src/result.js
+++ src/result.js
@@ -1,7 +1,20 @@
+
 import React from "react";
 
-function Result () {
-    <h3>Guess the number</h3>
-}
+function Result({term,secertNum}) {
+    let result;
+    
 
+    if(term) {
+      if(term > secertNum) {
+        result ='Higher';
+      }else if(term < secertNum){
+        result ='lower';
+      }else if(term === secertNum){
+      result = "wow! correct";
+      }        
+    }
+     return <h3> you guessed: {result}</h3>
+}
+   
 export default Result;
